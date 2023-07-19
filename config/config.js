d2504a858b827c6e15469b88ea6328dd926b1cfe
evaaaaawu <yiju.wu1@gmail.com>
2023-07-19 19:53:49 +0800

    fix: AWS cannot db:migrate problem

---
+++ config/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: 'password',
+    database: 'ac_twitter_workspace',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  test: {
+    username: 'root',
+    password: 'password',
+    database: 'ac_twitter_workspace_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    logging: false
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
+  },
+  travis: {
+    username: 'travis',
+    database: 'ac_twitter_workspace_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    logging: false
+  }
+}
